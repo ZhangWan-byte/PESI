0ec9f492d8f6c78f3650b470ac8e7c86a52641c8
ZhangWan-byte <zhang_wanzhang@foxmail.com>
2023-08-13 21:51:28 +0800

    modified directry structure -- 1st

--- cov_train.sh
+++
@@ -1,9 +0,0 @@
-# declare -a arr=('masonscnn' 'lstm' 'textcnn' 'ag_fast_parapred' 'pipr' 'resppi' 'pesi')
-declare -a arr=('pesi')
-
-for modelname in "${arr[@]}"
-do
-    echo "python cov_train.py $modelname $1 > ./logs/logs_${modelname}_$1 2>&1;"
-
-    python cov_train.py $modelname $1 > ./logs/logs_${modelname}_$1 2>&1;
-done
--- pre_train.sh
+++
@@ -1,9 +0,0 @@
-# declare -a arr=('masonscnn' 'lstm' 'textcnn' 'ag_fast_parapred' 'pipr' 'resppi' 'pesi')
-declare -a arr=('pesi')
-
-for model_name in "${arr[@]}"
-do
-    echo "python pre_train.py $model_name > ./logs/log_pretrain_$model_name 2>&1;"
-
-    python pre_train.py $model_name > ./logs/log_pretrain_$model_name 2>&1;
-done
